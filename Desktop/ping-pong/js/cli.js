a0f93c5583e364be67932a47ecdee2e7cb4a0497
JimKlein325 <j.mesklein@gmail.com>
2016-06-06 19:43:47 -0700

    add browserify and gulp build support.

--- Desktop/ping-pong/js/cli.js
+++ Desktop/ping-pong/js/cli.js
@@ -1,7 +1,23 @@
+
 var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
+
+prompt.get('goal', function(err, result) {
+  var result = pingPong(result.goal);
+  result.forEach(function(element) {
+    console.log(element);
+  });
+});
+
+
+/*var
+Original code
+pingPong = require('./ping-pong.js').pingPong;
 
 console.log('Ping-pong up to 100');
 var result = pingPong(100);
 result.forEach(function(element){
   console.log(element);
 });
+*/
